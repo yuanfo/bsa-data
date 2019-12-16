e4ee7778a8cc695e9b0139823f2779b30dbe586c
Louie Qin - Mac Air <yongrui.qin@gmail.com>
2019-12-16 12:43:33 +0000

    添加了智悲原文（主要是首页内容）

--- code/very_small_html_files.py
+++ code/very_small_html_files.py
@@ -1,13 +1,19 @@
 import os, sys
 # -*- coding: utf-8 -*-
-from bs4 import BeautifulSoup
-src_dir = sys.argv[1] if len(sys.argv) >= 2 else '../html'
+
+src_dir = sys.argv[1] if len(sys.argv) >= 2 else '../'
 dirpath = os.getcwd()
-log_files = dirpath+"/small_files_result.txt"
+log_files = dirpath+"/result.txt"
 count = 0
 fw = open(log_files, "w")
 print(src_dir)
 for root, dirs, files in os.walk(src_dir):
+	if 'code' in root or '../.' in root:
+		continue
+	# new_dir = 'zhibei/'+root.replace('../','')
+	# print(new_dir)
+	# if not os.path.exists(new_dir):
+	# 	os.makedirs(new_dir)
 	count += len(files)
 	for file in files:
 		filepath = os.path.join(root, file)
